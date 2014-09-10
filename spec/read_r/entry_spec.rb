dc4a4d6067b76a393d5e80335541bacc447524aa
GB Hoyt <qrprat77@gmail.com>
2014-09-10 01:01:04 -0400

    writing more specs, some are pending, some are failing

--- spec/read_r/entry_spec.rb
+++ spec/read_r/entry_spec.rb
@@ -4,10 +4,22 @@ require 'spec_helper'
 module Read_r
  
 	describe "Entry" do
-		entry = Read_r::Entry.new(name: 'Emma', recorder: 'qrprat77@gmail.com', minreadmon: 10, favebook: 'Fantastic Mr Fox', fiction: true)
+		entry = Read_r::Entry.new(name: 'Emma', recorder: 'qrprat77@gmail.com', minreadmon: 10, favbook: 'Fantastic Mr Fox', fiction: true)
 			
 		it "has the following minimum data, a name, a recorder, minutes during at least one day during the week, book that was read, and whether it is fiction or nonfiction" do
 			expect(entry.name).not_to eq(nil)
+		end
+		
+		it "has a method called :save" do
+			expect(entry).to respond_to(:save)
+		end
+		
+		it "saves to an xlsx file that is passed to save" do
+			pending("Need to decide if xlsx is right, and what gems to install")
+		end
+		
+		it "has a method called :current that displays the last values of entry for the current user" do
+			expect(entry).to respond_to(:current)
 		end
 		
 	end # Describe Entry
