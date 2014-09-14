3340f0d0e77cf0e17283452c32b78562305b4758
GB Hoyt <qrprat77@gmail.com>
2014-09-14 14:58:40 -0400

    Failing spec that requires I flesh out the spreadsheet class.

--- spec/read_r/entry_spec.rb
+++ spec/read_r/entry_spec.rb
@@ -2,38 +2,41 @@ require 'spec_helper'
 
 
 module Read_r
- 
+   
 	describe "Entry" do
-		entry = Read_r::Entry.new(name: 'Emma', recorder: 'qrprat77@gmail.com', minreadmon: 10, favbook: 'Fantastic Mr Fox', fiction: true)
-			
-		it "has the following minimum data, a name, a recorder, minutes during at least one day during the week, book that was read, and whether it is fiction or nonfiction" do
-			expect(entry.name).not_to eq(nil)
+		before :all do
+			@entry = Read_r::Entry.new(name: 'Emma', recorder: 'qrprat77@gmail.com', minreadmon: 10, favbook: 'Fantastic Mr Fox', fiction: true)
+			@xlsxfile = Read_r::Spreadsheet.new(rdr_list: 'raffertyclass.xlsx')
 		end
-		
-		it "has a method called :save" do
-			expect(entry).to respond_to(:save)
+			
+		it "has a name" do
+			expect(@entry.name).not_to eq(nil)
 		end
 		
 		it "has a book saved as a favbook" do
-			expect(entry.favbook).not_to eq(nil)
+			expect(@entry.favbook).not_to eq(nil)
 		end
-			
+		
+		it "has a method called :save" do
+			expect(@entry).to respond_to(:save)
+		end		
 		
 		it "saves to an xlsx file that is passed to save" do
-			pending("Need to decide if xlsx is right, and what gems to install")
+			@entry.save(@xlsxfile)
+			expect(@xlsxfile.last).to eq(@entry.current)
 		end
 		
 		it "has at least one record of the number of minutes read" do
 			days_with_mins = []
 			["mon", "tues", "weds", "thurs", "oth"].each do |day| 
 				methid = ("minread#{day}").to_sym
-				days_with_mins.push entry.send(methid) if entry.send(methid) != nil
+				days_with_mins.push @entry.send(methid) if @entry.send(methid) != nil
 				expect(days_with_mins.size).to be >(0)
 			end
 		end
 		
 		it "has a method called :current" do
-			expect(entry).to respond_to(:current)
+			expect(@entry).to respond_to(:current)
 		end
 		it "the :current method displays the last values of entry saved for the current user" do
 		  pending("Need to figure out if I need to know the last entry saved, or if this is redundant data.")
