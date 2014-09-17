c4451d133b243b5194b31eae6bffc805001e1b8d
GB Hoyt <qrprat77@gmail.com>
2014-09-17 07:56:55 -0400

    Failing tests abound! Need to discover how to correctly write specs for testing file path, etc.

--- spec/read_r/spreadsheet_spec.rb
+++ spec/read_r/spreadsheet_spec.rb
@@ -7,8 +7,19 @@ module Read_r
 	before :all do
 		@spreadsheet = Read_r::Spreadsheet.new(rdr_list: "Rafferty.xlsx")	
 	end
-		it "has a method called :last" do
-			expect(@spreadsheet).to respond_to(:last)
+		it "knows where the path for the spreadsheet is" do
+			pending "I dont know the best practice for testing this."
+		end
+		
+		describe "Last" do
+				it "has a method called :last" do
+					expect(@spreadsheet).to respond_to(:last)
+				end
+				
+				it "returns the last row of the spreadsheet as a string" do
+					expect(@spreadsheet.last).to eq("Emma qrprat77@gmail.com \"Fantastic Mr Fox\"  10   ")
+				end
+				
 		end
 	end
 
