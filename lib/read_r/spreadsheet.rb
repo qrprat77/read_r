548daf8ba79b7db2ce1f5018f55bad80f6f9b530
GB Hoyt <qrprat77@gmail.com>
2014-09-24 09:55:35 -0400

    Made a License, following MIT standards

--- lib/read_r/spreadsheet.rb
+++ lib/read_r/spreadsheet.rb
@@ -1,6 +1,5 @@
-# Spreadsheet class. May need a gem inheritance, 
-# for now defining to pass a feature step, and 
-# provide a framework for further development
+# Spreadsheet class. Create a local spreadsheet manager object
+# Use the rubyXL gem to manipulate spreadsheets.
 module Read_r
 	class Spreadsheet 
 		require 'rubyXL'
@@ -9,11 +8,22 @@ module Read_r
 		
 		def initialize(options = {}) 
 			@rdr_list = options[:rdr_list]
+			/* @spreadsheet = RubyXL::Parser.parse(filepath to @rdr_list)
+			Blah, need help with this. Where does the file go?
+			How do I know it's where the file goes?
+			How do I even test that? Is that an implementation issue?
+			Should I raise an exception if I try to open a file that 
+			doesn't exist? I know that in how I IMPLEMENT the library 
+			some users will be able to create and some won't. I think
+			that I can definately say that opening a sheet should only 
+			occur if the sheet exists. So error raised if we create this 
+			object*/
 			
 		end
 		
 		def last
-			#returns the last row of the spreadsheet.
+			last_row = self.last
+			last_row.join
 			
 		end
 		
