c73e7c11df6e74f12fd9c94eb4eb1046d717ed91
GB Hoyt <qrprat77@gmail.com>
2014-09-25 23:48:44 -0400

    simple edits made

--- lib/read_r/spreadsheet.rb
+++ lib/read_r/spreadsheet.rb
@@ -4,21 +4,24 @@ module Read_r
 	class Spreadsheet 
 		require 'rubyXL'
 		
+		
 		attr_accessor :rdr_list
+		PATHNAME
 		
 		def initialize(options = {}) 
 			@rdr_list = options[:rdr_list]
-=begin			/* @spreadsheet = RubyXL::Parser.parse(filepath to @rdr_list)
-			Blah, need help with this. Where does the file go?
-			How do I know it's where the file goes?
-			How do I even test that? Is that an implementation issue?
-			Should I raise an exception if I try to open a file that 
-			doesn't exist? I know that in how I IMPLEMENT the library 
-			some users will be able to create and some won't. I think
-			that I can definately say that opening a sheet should only 
-			occur if the sheet exists. So error raised if we create this 
-			object*/
-=end			
+			check_for_spreadsheet(@rdr_list)
+			#@spreadsheet = RubyXL::Parser.parse(filepath to @rdr_list)
+			#Blah, need help with this. Where does the file go?
+			#How do I know it's where the file goes?
+			#How do I even test that? Is that an implementation issue?
+			#Should I raise an exception if I try to open a file that 
+			#doesn't exist? I know that in how I IMPLEMENT the library 
+			#some users will be able to create and some won't. I think
+			#that I can definately say that opening a sheet should only 
+			#occur if the sheet exists. So error raised if we create this 
+			#object
+		
 		end
 		
 		def last
@@ -27,5 +30,8 @@ module Read_r
 			
 		end
 		
+		def check_for_spreadsheet(sheet)
+			false unless 
+		end
 	end # Spreadsheet class
 end
--- spec/read_r/spreadsheet_spec.rb
+++ spec/read_r/spreadsheet_spec.rb
@@ -5,19 +5,19 @@ module Read_r
 
 	describe "Spreadsheet" do
 	before :all do
-		@spreadsheet = Read_r::Spreadsheet.new(rdr_list: "Rafferty.xlsx")	
+		@sheet = Read_r::Spreadsheet.new(rdr_list: "Rafferty.xlsx")	
 	end
 		it "knows where the path for the spreadsheet is" do
-			pending "I dont know the best practice for testing this."
+			expect(@sheet.check_for_spreadsheet(@sheet.rdr_list)).to be_true
 		end
 		
 		describe "Last" do
 				it "has a method called :last" do
-					expect(@spreadsheet).to respond_to(:last)
+					expect(@sheet).to respond_to(:last)
 				end
 				
 				it "returns the last row of the spreadsheet as a string" do
-					expect(@spreadsheet.last).to eq("Emma qrprat77@gmail.com \"Fantastic Mr Fox\"  10   ")
+					expect(@sheet.last).to eq("Emma qrprat77@gmail.com \"Fantastic Mr Fox\"  10   ")
 				end
 				
 		end
